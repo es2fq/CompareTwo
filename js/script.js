837d785e0ffba0c4c689b125d42981d188c7ed65
Erik Song <es2fq@virginia.edu>
2017-06-18 02:40:32 -0400

    reformatted layout of page 1 and added croppie library

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
+    var pageSelector = '#pages > div';
     var menuSelector = '#topnav ul li';
     $(menuSelector).on('click', function() {
         $(menuSelector).removeClass('active');
         $(this).addClass('active');
+
+        var pageId = "#page" + ($(this).index() + 1);
+        $(pageSelector).removeClass('active');
+        $(pageId).addClass('active');
     });
 
+    var offset = 9;
     resizeImageWindow();
-    window.onresize = resizeImageWindow;
+    offset = -6;
+
+    $(window).on('resize', resizeImageWindow);
 
     function resizeImageWindow() {
+        console.log("resize");
         var imageWindow = '#page1';
-        var width = $(imageWindow).css('width');
-        var newHeight = parseFloat(width, 10) * (2/3); 
+        var navigationBar = '#topnav';
+        var newWidth = parseFloat($(navigationBar).css('width')) + offset;
+        var newHeight = parseFloat(newWidth, 10) * (14/30); 
+        $(imageWindow).css('width', newWidth);
         $(imageWindow).css('height', newHeight);
     }
 });
