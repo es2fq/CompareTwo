fec6602c15ab1c48b61a0fb688c64509d1b6ce27
Erik Song <es2fq@virginia.edu>
2017-06-19 18:03:05 -0400

    added croppie code to js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,6 @@ $(document).ready(function() {
     $(window).on('resize', resizeImageWindow);
 
     function resizeImageWindow() {
-        console.log("resize");
         var contentWindow = '.page';
         var navigationBar = '#topnav';
         var newWidth = parseFloat($(navigationBar).css('width')) + offset;
@@ -25,4 +24,27 @@ $(document).ready(function() {
         $(contentWindow).css('width', newWidth);
         $(contentWindow).css('height', newHeight);
     }
+
+    var leftImage = $('#page2 #left #image1');
+    var leftDim = parseFloat(leftImage.css('width')) / 2;
+    var rightImage = $('#page2 #right #image2');
+    var rightDim = parseFloat(rightImage.css('width')) / 2;
+
+    var leftCrop = leftImage.croppie({
+        viewport: {
+            width: leftDim,
+            height: leftDim,
+        }
+    });
+    var rightCrop = rightImage.croppie({
+        viewport: {
+            width: rightDim,
+            height: rightDim,
+        }
+    });
+
+    var imageUrl = "https://assets.rbl.ms/1268721/980x.jpg";
+    leftCrop.croppie('bind', {
+        url: "https://crossorigin.me/" + imageUrl
+    });
 });
