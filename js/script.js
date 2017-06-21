67944d6b90fd04a019becd63eea14a93b1db1f77
Erik Song <es2fq@virginia.edu>
2017-06-21 08:56:50 -0400

    added centering in jquery

--- js/script.js
+++ js/script.js
@@ -25,26 +25,41 @@ $(document).ready(function() {
         $(contentWindow).css('height', newHeight);
     }
 
+    var leftImport = $('#page2 #left');
+    var rightImport = $('#page2 #right');
+
     var leftImage = $('#page2 #left #image1');
-    var leftDim = parseFloat(leftImage.css('width')) / 2;
+    var leftViewportDim = parseFloat(leftImage.css('width')) / 2;
     var rightImage = $('#page2 #right #image2');
-    var rightDim = parseFloat(rightImage.css('width')) / 2;
+    var rightViewportDim = parseFloat(rightImage.css('width')) / 2;
 
     var importButton1 = $('#importImg1');
     var importButton2 = $('#importImg2');
 
     // var leftCrop = leftImage.croppie({
     //     viewport: {
-    //         width: leftDim,
-    //         height: leftDim,
+    //         width: leftViewportDim,
+    //         height: leftViewportDim,
     //     }
     // });
     // var rightCrop = rightImage.croppie({
     //     viewport: {
-    //         width: rightDim,
-    //         height: rightDim,
+    //         width: rightViewportDim,
+    //         height: rightViewportDim,
     //     }
     // });
+    
+    leftImageDim = parseFloat(leftImport.width() / 2);
+    leftImage.css('width', leftImageDim);
+    leftImage.css('height', leftImageDim); 
+    leftImage.css('margin-left', '25%');
+    leftImage.css('margin-top', (parseFloat(leftImport.css('height')) - leftImageDim) / 2);
+
+    rightImageDim = parseFloat(rightImport.width() / 2);
+    rightImage.css('width', rightImageDim);
+    rightImage.css('height', rightImageDim); 
+    rightImage.css('margin-left', '25%');
+    rightImage.css('margin-top', (parseFloat(rightImport.css('height')) - rightImageDim) / 2);
 
     // var imageUrl = "https://assets.rbl.ms/1268721/980x.jpg";
     // leftCrop.croppie('bind', {
