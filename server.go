73f1daf265b609ba704ed64c66bb7050a2cdcf8a
Erik Song <es2fq@virginia.edu>
2017-07-07 10:56:03 -0400

    might work but cant test bc proxy

--- server.go
+++ server.go
@@ -12,14 +12,32 @@ import (
 	"strings"
 )
 
+const (
+	dbhost = "ec2-23-21-96-159.compute-1.amazonaws.com"
+	dbname = "d3uihtml34nn49"
+	dbuser = "cjygjjffrhrhrd"
+	dbpass = "17d9939428b880bf2022612976c8098b9fbd315eecd619d16ff7266b23cb1fce"
+	dbport = 5432
+)
+
 var db *sql.DB
 var templates = template.New("")
 
 func initializeDatabase() {
 	var err error
-	db, err = sql.Open("postgres", "ec2-23-21-96-159.compute-1.amazonaws.com")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbhost, dbport, dbuser, dbpass, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+
 	if err != nil {
 		log.Fatalf("Error opening database: %q", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("Error on ping: %q", err)
 	}
 }
 
