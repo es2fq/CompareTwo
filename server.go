08809294aea4209945d0a094d6c684283114b9fd
Erik Song <es2fq@virginia.edu>
2017-07-12 08:48:20 -0400

    sql test

--- server.go
+++ server.go
@@ -103,6 +103,12 @@ func main() {
 	initializeDatabase()
 	initializeTables()
 
+	stmt, err := db.Prepare("INSERT INTO Posts (Question, Desc1, Desc2, Image1, Image2) VALUES (?, ?, ?, ?, ?)")
+	checkError(err)
+
+	res, err := stmt.Exec("asdf", "hello", "jiojw", "vawieop", "fjwfo")
+	checkError(err)
+
 	// result, err := db.Query("SELECT * FROM Posts")
 	// checkError(err)
 
