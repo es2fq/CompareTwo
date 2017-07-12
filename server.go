ea09a4a7aa2d3ef3c517a0b5f9e1de3a69a97651
Erik Song <es2fq@virginia.edu>
2017-07-12 09:16:04 -0400

    more testing

--- server.go
+++ server.go
@@ -105,6 +105,7 @@ func main() {
 
 	stmt, err := db.Prepare("INSERT INTO Posts (Question, Desc1, Desc2, Image1, Image2) VALUES (?, ?, ?, ?, ?)")
 	checkError(err)
+	log.Println("After Prepare")
 
 	res, err := stmt.Exec("asdf", "hello", "jiojw", "vawieop", "fjwfo")
 	checkError(err)
