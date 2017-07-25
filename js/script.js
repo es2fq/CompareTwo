a0b5a31ff620da8b3877ffecba279c76de81bb3a
Erik Song <es2fq@virginia.edu>
2017-07-25 08:58:31 -0400

    more js testing

--- js/script.js
+++ js/script.js
@@ -52,9 +52,9 @@ $(document).ready(function() {
                             padding-left: 5%;
                             padding-right: 5%;
                         `;
-                        recentPost.on('click', function() {
+                        recentPost.onclick = function() {
                             console.log($(this).index());
-                        });
+                        };
                         postContainer.append(recentPost);
 
                         recentPosts.push({
