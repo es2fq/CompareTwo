e30a5a0ca96d4895a8247a7995e2c7da227ee87d
Erik Song <es2fq@virginia.edu>
2017-07-26 15:54:33 -0400

    fixed delete back button on reload

--- js/script.js
+++ js/script.js
@@ -440,6 +440,10 @@ $(document).ready(function() {
         newCanvas.id = "barGraph";
         chartArea.appendChild(newCanvas);
 
+        var backButton = document.createElement('div');
+        backButton.id = "backButton";
+        backButton.className = "hvr-grow";
+
         $('#page1 #chartArea').css('visibility', 'hidden');
         $('#page1 #chartArea').css('opacity', '0');
 
